817aa28ef520a00ddbcf43bdacad999a8c1dd7d6
Marc Avalos <mavalos@email.arizona.edu>
2019-12-19 19:59:26 -0700

    Boiler Plate Commit

---
+++ tournament-manager/project-3-mongoose-master/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+
+const userSchema = new Schema({
+	firstName: { type: String, unique: false },
+	lastName: { type: String, unique: false },
+	local: {
+		username: { type: String, unique: false, required: false },
+		password: { type: String, unique: false, required: false }
+	},
+	google: {
+		googleId: { type: String, required: false }
+	},
+	photos: []
+})
+
+userSchema.methods = {
+	checkPassword: function (inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.local.password)
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10)
+	}
+}
+
+// MUST HASH BEFORE SAVE
+userSchema.pre('save', function (next) {
+	if (!this.local.password) {
+		next()
+	} else {
+		this.local.password = this.hashPassword(this.local.password)
+		next()
+	}
+})
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
